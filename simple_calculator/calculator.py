93f32791aad771f71da182bebb04eafbc0b66661
Veera Venkata Rama Krishna <97684459+avvrkrishna@users.noreply.github.com>
2022-02-19 22:56:36 +0530

    Update calculator.py

--- simple_calculator/calculator.py
+++ simple_calculator/calculator.py
@@ -42,4 +42,4 @@ elif choice == 'c':
 elif choice == 'd':    
    print (num_1, " / ", num_2, " = ", divide(num_1, num_2))    
 else:    
-   print ("This is an invalid input")
+   print ("Invalid input")
